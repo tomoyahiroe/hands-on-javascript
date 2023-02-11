cc721efd77b682111c56bfbe5f8b84139db0f64c
tomoyahiroe <tomoya4creative@gmail.com>
2023-02-11 21:04:18 +0900

    initial commit

---
+++ basic-grammar.js
@@ -0,0 +1,83 @@
+// ハンズオンJSサンプルコード(RunJSで使ってね)
+
+// 1. 定数と変数の宣言
+
+// 1-1. 宣言
+// const string = 'Johny'; //文字列型の代入
+// let number = 24; // 数値型の代入
+// const array = [1, 2, 3, 4, 5]; // 配列の代入
+// const person = {
+//   //オブジェクトの代入
+//   name: 'John',
+//   age: 24,
+//   hobby: ['running', 'reading', 'programming'],
+//   function: () => {
+//     console.log("Hello World")
+//   }
+// };
+
+// 1-2. 定数(const)は変更不可
+// string = "Alex" // TypeError: Assignment to constant variable.
+
+// 1-3. 変数(let)は再代入可能
+// number = 35
+
+// 1-4演算と論理
+// 1 + 3 - 4
+// 2 * 3 / 6
+// let num = 1
+// ++num // インクリメント
+// 1 > 2 // 論理値が返る
+// 1 === 1
+
+// 2. 条件分岐
+// const templature = 30
+// if (templature > 25) {
+//   console.log("暑い")
+// } else {
+//   console.log("暑くない")
+// }
+
+// 3. 繰り返し
+// for (let i = 0; i < 10;i++) {
+//   console.log(i)
+// }
+
+// 4. 関数
+// function sum(x, y) {
+//   return x + y
+// }
+// sum(2,4)
+
+// function sumArray(array) {
+//   const length = array.length
+//   let result = 0;
+//   for (let i=0;i<length;i++) {
+//     result = result + array[i]
+//   }
+//   return result
+// }
+// const testScore = [90, 80, 70, 88, 12]
+// sumArray(testScore)
+
+// 5. 配列をランダムにシャッフルしよう
+// 5-1 math()
+// Math.E //ネイピア数
+// Math.random() // 0〜1のランダムな数を生成
+
+// 5-2 1〜10をランダムに出力する
+// Math.floor(10 * Math.random()) + 1
+
+// 5-3 
+// const perticipants = ["Ito", "Kubo", "Shion", "Kojima","Sunguk", "Tomoya"]
+// const numberOfPerticipants = perticipants.length
+// const randomIndex = Math.floor(numberOfPerticipants * Math.random()) + 1
+
+
+
+
+
+
+
+
+
