11d1202619ce4a6523b9047136c43e27dc5c2864
tomoyahiroe <tomoya4creative@gmail.com>
2023-02-14 16:44:47 +0900

    reinitialize

--- basic-grammar.js
+++ basic-grammar.js
@@ -23,15 +23,17 @@
 // number = 35
 
 // 1-4演算と論理
-// 1 + 3 - 4
-// 2 * 3 / 6
+// 1 + 3 - 57
+// const one = 1;
+// const two = 2;
+// one + two
+// 2 * 3 /2
 // let num = 1
-// num += 1 // インクリメント for(;; i++) 
-// num = num - 1
-// 1 > 2 // 論理値が返る
-// 1 === "1" //厳格等価演算子
+// num += 1 // インクリメント for(;; i++)
+// num = num + 1
+// 1 < 2 // 論理値が返る boolean
+// 1 === "1" //厳格等価演算子 
 // 1 == "1"
-
 
 // 2. 条件分岐
 // const templature = 0
@@ -48,26 +50,26 @@
 // }
 
 // 3. 繰り返し
-// console.log("おはよう") 
+// console.log("おはよう")
 // const a = "あ"
 // console.log(a)
 // for (初期化式;条件式;更新式){}
 // for (let i = 0; i < 10;i++) {
-//   console.log("iihara")
+//   console.log(i, "iihara")
 // }
 
 // const arr = [1,2,3,4,5]
-// for (let i = 0,len = [1,2,3,4,5].length; i < len;i++) {
-//   console.log("heyheyhoo")
-//   console.log(len)
+// for (let i = 0,len = arr.length; i < len;i++) {
+//   console.log(arr[i])
+  // console.log(len)
 // }
 // 4. 関数
 // function sum(x, y) {
-//   const z = x**2 + y*2 + 1
+//   const z = x + y
 //   return z
 // }
 // sum(3,4)
-
+// sum(3,8)
 
 // function sumArray(array) {
 //   const length = array.length
@@ -77,41 +79,41 @@
 //   }
 //   return result
 // }
-// const testScore = [90, 80, 70, 88, 12]
+// const testScore = [90, 80, 70, 88, 12,100]
 // sumArray(testScore)
 // 90 + 80 + 70 +88 + 12
 
 // 5. 配列をランダムにシャッフルしよう
-// 5-1 math()
-// Math.E //ネイピア数
+// 5-1 Math() //組み込みオブジェクト
+// Math.floor(1.9)//ネイピア数
 // Math.random() // 0〜1のランダムな数を生成
 
 // 5-2 1〜10をランダムに出力する
-// Math.floor(10 * Math.random()) + 1 
+// Math.floor(10 * Math.random()) + 1
 // for (let i =0; i < 100; i++) {
 //   console.log(Math.floor(10 * Math.random()) + 1)
 // }
 
-// 5-3 
+// 5-3 席替えプログラム
 // const perticipants = ["Ito", "Kubo", "Shion", "Kojima","Sunguk", "Tomoya", "Koichi", "Yata"]
 // const numberOfPerticipants = perticipants.length
 // const randomIndex = Math.floor(numberOfPerticipants * Math.random())
 
-// for (let i = 0; i < perticipants.length; i++) {
+// for (let i = 0; i < 100; i++) {
 //   const randomIndex = Math.floor(perticipants.length * Math.random());
 //   //問題のコード
 //   [perticipants[i], perticipants[randomIndex]] = [perticipants[randomIndex], perticipants[i]]
 // }
 // console.log(perticipants)
 
-// const perticipants = ["Ito", "Kubo", "Shion", "Kojima","Sunguk", "Tomoya", "Koichi", "Yata"]
-// let shuffledArray = []
+const perticipants = ["Ito", "Kubo", "Shion", "Kojima","Sunguk", "Tomoya", "Koichi", "Yata"]
+let shuffledArray = []
 // for (;shuffledArray.length < perticipants.length;) {
 //   const numberOfPerticipants = perticipants.length
 //   const randomIndex = Math.floor(numberOfPerticipants * Math.random())
 //   if (!shuffledArray.includes(perticipants[randomIndex])) {
 //     shuffledArray.push(perticipants[randomIndex]);
-//   }
+//   } 
 // }
 // console.log(shuffledArray)
 
@@ -123,6 +125,13 @@
 //   }
 // }
 // console.log(shuffledArray)
+
+
+
+
+
+
+
 
 
 
